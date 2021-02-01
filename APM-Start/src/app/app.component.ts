78f9dd87c42d897860c8b1b32ad40b08f4341668
Dominik Sasko <dominik.sasko@siemens-healthineers.com>
2021-02-01 16:48:45 +0100

    Added list of products + filter feature.

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,20 @@
+// import { Component } from '@angular/core';
+
 import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title: string = 'Kurvoland';
 }
